5920dc102824248ef272f0765e5ddbd549c545b2
goodseog <goodseog.yoo@samsung.com>
2019-12-30 15:10:39 +0900

    [Backend] Style change using Flask_restful

--- bamboo/app.py
+++ bamboo/app.py
@@ -1,16 +1,14 @@
 from flask import Flask, render_template
+from flask_restful import Api
+import pandas as pd
+
+from api.root import RootAPI
 
 app = Flask(__name__)
+api = Api(app)
 
-
-@app.route('/api')
-def web():
-    data = {
-        'title': 'Title',
-        'body': 'my body!!'
-    }
-    return data
-
+api.add_resource(RootAPI, '/api')
+# api.add_resource(RootAPI, '/api/<argument:type>')
 
 if __name__ == '__main__':
     app.run(host='127.0.0.1', port=3001)
