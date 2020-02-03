339524ae8dbbfdbe2d2e7b960860e329314281d6
goodseog <goodseog.yoo@samsung.com>
2020-02-03 09:51:56 +0900

    indent fix

--- bamboo/src/components/mainboard/MainBoard.js
+++ bamboo/src/components/mainboard/MainBoard.js
@@ -18,8 +18,19 @@ class MainBoard extends React.Component {
     return (
       <StyledMainBoard>
         {/* {this.props.nodes.map(node => <MainBoardNode />)} */}
-        <iframe id='a0a2a8da' name='a0a2a8da' src='http://10.113.66.107/adserver/www/delivery/afr.php?zoneid=1&amp;target=_blank&amp;cb=INSERT_RANDOM_NUMBER_HERE' frameborder='0' scrolling='no' width='180' height='200' allow='autoplay'><a href='http://10.113.66.107/adserver/www/delivery/ck.php?n=a35f4287&amp;cb=INSERT_RANDOM_NUMBER_HERE' target='_blank'><img src='http://10.113.66.107/adserver/www/delivery/avw.php?zoneid=1&amp;cb=INSERT_RANDOM_NUMBER_HERE&amp;n=a35f4287' border='0' alt='' /></a></iframe>
-
+        <iframe 
+          id='a0a2a8da' 
+          name='a0a2a8da' 
+          title='banner'
+          src='http://10.113.66.107/adserver/www/delivery/afr.php?zoneid=1&amp;target=_blank&amp;cb=INSERT_RANDOM_NUMBER_HERE'
+          frameborder='0' 
+          scrolling='no' 
+          width='180' height='200' 
+          allow='autoplay'>
+          <a href='http://10.113.66.107/adserver/www/delivery/ck.php?n=a35f4287&amp;cb=INSERT_RANDOM_NUMBER_HERE' target='_blank'>
+            <img src='http://10.113.66.107/adserver/www/delivery/avw.php?zoneid=1&amp;cb=INSERT_RANDOM_NUMBER_HERE&amp;n=a35f4287' border='0' alt='' />
+          </a>
+        </iframe>
       </StyledMainBoard>
     )
   }
